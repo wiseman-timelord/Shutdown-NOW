7d296cc336e45a0f0425e198e20088dd6f93a41c
Ma'Star Wiseman-Timelord <92948927+wiseman-timelord@users.noreply.github.com>
2025-06-02 01:44:55 +0100

    Add files via upload

--- Shutdown_NOW.bat
+++ Shutdown_NOW.bat
@@ -3,10 +3,9 @@ color 1F
 title SYSTEM SHUTDOWN
 
 :: Stylized header
-echo.
-echo =====================================================
-echo               *** SYSTEM SHUTDOWN ***
-echo =====================================================
+echo ========================================================================================================================
+echo                                                *** SYSTEM SHUTDOWN ***
+echo ========================================================================================================================
 echo.
 echo    Method: Forced Immediate Shutdown
 echo    Reason: Shutdown Batch Was Executed.
@@ -19,9 +18,15 @@ echo Executing Command...
 shutdown /f /p
 echo ...Command Executed.
 timeout /t 1 /nobreak >nul
-echo Wait Up To 10 Mins For Shutdown. If no Power Off After 10 Min, then Alt+Ctrl+Esc and terminate fishy processes, like updaters.
-timeout /t 5 /nobreak >nul
+echo.
+echo 1) Wait Up To 10 Mins For Shutdown. 
+echo 2) After 10 Min, terminate fishy processes.
+echo.
+timeout /t 1 /nobreak >nul
 
 ::ExitSequence
+echo Exiting In 5 Seconds...
+timeout /t 5 /nobreak >nul
 exit
+
 
